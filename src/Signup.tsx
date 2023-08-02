0d6a0c38033968f5944405c924a50e20bf4478d2
nagyb3 <nagybence2003@gmail.com>
2023-08-02 23:58:01 +0200

    username and password confirm validator

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -3,31 +3,57 @@ import React from 'react'
 export default function Signup() {
   const [usernameInput, setUsernameInput] = React.useState('')
   const [passwordInput, setPasswordInput] = React.useState('')
+  const [passwordConfirmInput, setPasswordConfirmInput] = React.useState('')
   const [emailInput, setEmailInput] = React.useState('')
+  const [usernameErrorState, setUsernameErrorState] = React.useState<null | string>(null)
+  const [passwordErrorState, setPasswordErrorState] = React.useState<null | string>(null)
 
   const fetchSignup = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    fetch(`${import.meta.env.VITE_API_URL}/signup`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: usernameInput,
-        password: passwordInput,
-        email: emailInput
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          window.location.href = '/login'
-        }
+    if (passwordInput !== passwordConfirmInput) {
+      setPasswordErrorState('The two password have to be equal!')
+      return
+    } else {
+      fetch(`${import.meta.env.VITE_API_URL}/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: usernameInput,
+          password: passwordInput,
+          email: emailInput,
+        }),
       })
-      .then((data) => {})
-      .catch((error) => {
-        console.error(error)
-      })
+        .then((response) => {
+          if (response.ok) {
+            window.location.href = '/login'
+          }
+        })
+        .then((data) => {})
+        .catch((error) => {
+          console.error(error)
+        })
+    }
   }
+
+  React.useEffect(() => {
+    if (usernameInput !== '') {
+      fetch(`${import.meta.env.VITE_API_URL}/users/exists/${usernameInput}`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          if (data.exists === true) {
+            setUsernameErrorState('Username already exists!')
+          } else {
+            setUsernameErrorState('')
+          }
+        })
+    } else {
+      setUsernameErrorState('')
+    }
+  }, [usernameInput])
 
   return (
     <div className="bg-orange-100 h-[calc(100vh-70px)]">
@@ -72,6 +98,20 @@ export default function Signup() {
             onChange={(e) => setPasswordInput(e.target.value)}
             value={passwordInput}
           />
+        </div>
+        <div className="w-fit">
+          <label htmlFor="repeat-password" className="text-lg">
+            Repeat password:
+          </label>
+          <input
+            className="mx-3 p-1 border-[1px] border-black rounded"
+            type="password"
+            id="repeat-password"
+            onChange={(e) => setPasswordConfirmInput(e.target.value)}
+            value={passwordConfirmInput}
+          />
+          <p className="text-center text-red-600 mt-6 text-lg">{usernameErrorState}</p>
+          <p className="text-center text-red-600 mt-6 text-lg">{passwordErrorState}</p>
         </div>
         <input
           type="submit"
